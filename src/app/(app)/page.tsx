99e7f24bb5130d86b1c78724da1d4dce7e9ba86f
lbavendanos <lbavendanos@gmail.com>
2024-08-02 20:28:24 -0500

    Fix home page

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,3 +1,9 @@
 export default function HomePage() {
-  return <div>Home Page</div>
+  return (
+    <div className="container my-6 md:my-10 lg:my-12">
+      <section className="grid grid-cols-12 items-center gap-6">
+        <div className="col-span-12">Home Page</div>
+      </section>
+    </div>
+  )
 }
