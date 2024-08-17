17db45c64437d6e45ff35027573507aba27d4016
lbavendanos <lbavendanos@gmail.com>
2024-08-16 19:15:29 -0500

    Add boolean utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -20,3 +20,44 @@ export function cn(...inputs: ClassValue[]): string {
 export function url(path: string = '/'): URL {
   return new URL(path, process.env.NEXT_PUBLIC_APP_URL)
 }
+
+/**
+ * Check if a value is a boolean.
+ *
+ * @param {any} value - The value to check.
+ * @returns {boolean} Returns `true` if the value is a boolean, `false` otherwise.
+ */
+export function boolean(value: any): boolean {
+  const type = Object.prototype.toString.call(value)
+
+  if (type === '[object String]')
+    return ['true', 't', 'yes', 'y', 'on', '1'].includes(
+      value.trim().toLowerCase(),
+    )
+
+  if (type === '[object Number]') return value === 1
+
+  if (type === '[object Boolean]') return value
+
+  return false
+}
+
+/**
+ * Check if a value is `true`.
+ *
+ * @param {any} value - The value to check.
+ * @returns {boolean} Returns `true` if the value is `true`, `false` otherwise.
+ */
+export function isTrue(value: any): boolean {
+  return boolean(value)
+}
+
+/**
+ * Check if a value is `false`.
+ *
+ * @param {any} value - The value to check.
+ * @returns {boolean} Returns `true` if the value is `false`, `false` otherwise.
+ */
+export function isFalse(value: any): boolean {
+  return !isTrue(value)
+}
